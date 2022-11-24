2079bb4b025613d99c5f2dd861a7d8bb47143f9f
Behnam Salehi <salehinezhad.b@gmail.com>
2022-11-24 15:28:27 +0330

    move Comparers to their files

--- StockMarket.Domain/StockMarketProcessor.cs
+++ StockMarket.Domain/StockMarketProcessor.cs
@@ -1,38 +1,5 @@
 ﻿namespace StockMarket.Domain
 {
-    public abstract class BaseComparer : IComparer<Order>
-    {
-        public int Compare(Order? x, Order? y)
-        {
-            var result = SpecificCompare(x, y);
-            if (result != 0) return result;
-
-            if (y.Id < x.Id) return 1;
-            if (y.Id > x.Id) return -1;
-
-            return 0;
-        }
-
-        protected abstract int SpecificCompare(Order x, Order y);
-    }
-    public class MaxComparer : BaseComparer
-    {
-        protected override int SpecificCompare(Order x, Order y)
-        {
-            if (y.Price > x.Price) return 1;
-            if (y.Price < x.Price) return -1;
-            return 0;
-        }
-    }
-    public class MinComparer : BaseComparer
-    {
-        protected override int SpecificCompare(Order x, Order y)
-        {
-            if (y.Price < x.Price) return 1;
-            if (y.Price > x.Price) return -1;
-            return 0;
-        }
-    }
     public class StockMarketProcessor
     {
         private MarketState state;
