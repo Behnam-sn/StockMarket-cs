71fcc99c3187f1fdd9446fcca82ce99722e753be
Behnam Salehi <salehinezhad.b@gmail.com>
2022-11-24 15:30:12 +0330

    add comparers to PriorityQueue

--- StockMarket.Domain/StockMarketProcessor.cs
+++ StockMarket.Domain/StockMarketProcessor.cs
@@ -7,15 +7,13 @@
         private PriorityQueue<Order, Order> sellOrders;
         public StockMarketProcessor()
         {
-            buyOrders = new PriorityQueue<Order, Order>();
-            sellOrders = new PriorityQueue<Order, Order>();
+            buyOrders = new PriorityQueue<Order, Order>(new MaxComparer());
+            sellOrders = new PriorityQueue<Order, Order>(new MinComparer());
         }
-
         public void Open()
         {
             state = MarketState.Open;
         }
-
         public void EnqueueOrder(TradeSide side, decimal price, decimal quantity)
         {
             throw new NotImplementedException();
