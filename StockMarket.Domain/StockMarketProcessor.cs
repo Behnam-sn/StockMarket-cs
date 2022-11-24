18fdcc012bb7ef48bf8da0f1ff9a5735a9e9684a
Behnam Salehi <salehinezhad.b@gmail.com>
2022-11-24 15:21:15 +0330

    add MinComparer

--- StockMarket.Domain/StockMarketProcessor.cs
+++ StockMarket.Domain/StockMarketProcessor.cs
@@ -13,6 +13,19 @@
             return 0;
         }
     }
+    public class MinComparer : IComparer<Order>
+    {
+        public int Compare(Order? x, Order? y)
+        {
+            if (y.Price < x.Price) return 1;
+            if (y.Price > x.Price) return -1;
+
+            if (y.Id < x.Id) return 1;
+            if (y.Id > x.Id) return -1;
+
+            return 0;
+        }
+    }
     public class StockMarketProcessor
     {
         private MarketState state;
