5d85502f053d1deb2c3a119d6733d03cb62ee813
Behnam Salehi <salehinezhad.b@gmail.com>
2022-12-04 00:37:22 +0330

    add StockMarketState abstract class

---
+++ StockMarket.Domain/StockMarketState.cs
@@ -0,0 +1,20 @@
+﻿namespace StockMarket.Domain
+{
+    internal abstract class StockMarketState : IStockMarketProcessor
+    {
+        protected readonly StockMarketProcessor stockMarket;
+
+        public StockMarketState(StockMarketProcessor stockMarket)
+        {
+            this.stockMarket = stockMarket;
+        }
+        public virtual void Open()
+        {
+            throw new NotImplementedException();
+        }
+        public virtual long EnqueueOrder(TradeSide side, decimal price, decimal quantity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
