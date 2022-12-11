81608829db78d940b9d6caaaf62fa73dfca81444
Behnam Salehi <salehinezhad.b@gmail.com>
2022-12-11 03:50:13 +0330

    add test 1

--- StockMarket.Tests/BlockingCollectionTests.cs
+++ StockMarket.Tests/BlockingCollectionTests.cs
@@ -1,7 +1,39 @@
+using System.Collections.Concurrent;
+
 namespace StockMarket.Tests
 {
     public class BlockingCollectionTests
     {
+        [Fact]
+        public async Task AddTestAsync()
+        {
+            // Arrange
+            var sut = new BlockingCollection<int>();
+            // Act
+            var task1 = Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sut.Add(i);
+                    //await Task.Delay(100);
+                }
+                sut.CompleteAdding();
+            });
 
+            var sum = 0;
+            var task2 = Task.Run(() =>
+            {
+                while (!sut.IsAddingCompleted || sut.Count > 0)
+                {
+                    int item;
+                    if (!sut.TryTake(out item)) continue;
+                    sum += item;
+                }
+            });
+
+            await Task.WhenAll(task1, task2);
+            // Assert
+            Assert.Equal(45, sum);
+        }
     }
 }
