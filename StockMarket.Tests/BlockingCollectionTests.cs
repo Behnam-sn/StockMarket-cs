df15c7fb767295584c0a4d45eaa2fc353d1f20b3
Behnam Salehi <salehinezhad.b@gmail.com>
2022-12-11 22:53:01 +0330

    add test 2

--- StockMarket.Tests/BlockingCollectionTests.cs
+++ StockMarket.Tests/BlockingCollectionTests.cs
@@ -35,5 +35,50 @@ namespace StockMarket.Tests
             // Assert
             Assert.Equal(45, sum);
         }
+        struct QueueItem
+        {
+            public QueueItem(int data)
+            {
+                this.Data = data;
+                Completion = new TaskCompletionSource<int>();
+            }
+            public int Data { get; }
+            public TaskCompletionSource<int> Completion { get; }
+        }
+        [Fact]
+        public async void AddWithBlcokingProducerTestAsync()
+        {
+            // Arrange
+            var sut = new BlockingCollection<QueueItem>();
+            var tasks = new Task[11];
+            var j = -1;
+            var sum = 0;
+            // Act
+            for (int i = 0; i < 10; i++)
+            {
+                tasks[i] = Task.Run(async () =>
+                {
+                    var item = new QueueItem(Interlocked.Increment(ref j));
+                    sut.Add(item);
+
+                    if (j == 9) sut.CompleteAdding();
+
+                    var data = await item.Completion.Task;
+                    Interlocked.Add(ref sum, data);
+                });
+            }
+            tasks[10] = Task.Run(() =>
+            {
+                while (!sut.IsAddingCompleted || sut.Count > 0)
+                {
+                    QueueItem item;
+                    if (!sut.TryTake(out item)) continue;
+                    item.Completion.SetResult(item.Data + 1);
+                }
+            });
+            await Task.WhenAll(tasks);
+            // Assert
+            Assert.Equal(55, sum);
+        }
     }
 }
