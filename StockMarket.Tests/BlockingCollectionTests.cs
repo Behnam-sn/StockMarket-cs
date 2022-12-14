c44f87b8fd3fe2ff1c8a4467e0f7aa38c33865bb
Behnam Salehi <salehinezhad.b@gmail.com>
2022-12-15 01:04:12 +0330

    add test 3

--- StockMarket.Tests/BlockingCollectionTests.cs
+++ StockMarket.Tests/BlockingCollectionTests.cs
@@ -1,4 +1,6 @@
 using System.Collections.Concurrent;
+using StockMarket.Domain;
+using StockMarket.Domain.Commands;
 
 namespace StockMarket.Tests
 {
@@ -79,6 +81,58 @@ namespace StockMarket.Tests
             await Task.WhenAll(tasks);
             // Assert
             Assert.Equal(55, sum);
+        }
+        [Fact]
+        public async void AddWithBlcokingProducerAndStockMarketCommandTest()
+        {
+            // Arrange
+            var stockMarket = new StockMarketProcessor();
+            stockMarket.Open();
+            var sut = new BlockingCollection<Domain.Commands.ICommand>();
+            var orders = new BlockingCollection<long>();
+            var tasks = new Task[11];
+            var j = -1;
+            // Act
+            for (int i = 0; i < 5; i++)
+            {
+                tasks[i] = Task.Run(async () =>
+                {
+                    var item = new EnqueueCommand(stockMarket, TradeSide.Buy, Interlocked.Increment(ref j), 100M);
+                    sut.Add(item);
+
+                    var data = await item.WaitForCompletion();
+                    orders.Add(data);
+                });
+            }
+            for (int i = 5; i < 10; i++)
+            {
+                tasks[i] = Task.Run(async () =>
+                {
+                    var item = new CancelCommand(stockMarket, orders.Take());
+                    Interlocked.Increment(ref j);
+                    sut.Add(item);
+
+                    if (j == 9)
+                    {
+                        sut.CompleteAdding();
+                        orders.CompleteAdding();
+                    }
+                    var data = await item.WaitForCompletion();
+                });
+            }
+            tasks[10] = Task.Run(() =>
+            {
+                while (!sut.IsAddingCompleted || sut.Count > 0)
+                {
+                    Domain.Commands.ICommand item;
+                    if (!sut.TryTake(out item)) continue;
+                    item.Execute();
+                }
+            });
+
+            await Task.WhenAll(tasks);
+            // Assert
+            Assert.Equal(5, stockMarket.Orders.Count(o => o.IsCanceled));
         }
     }
 }
